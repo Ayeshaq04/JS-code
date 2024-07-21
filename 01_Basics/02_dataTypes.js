cf9862f9f00c22a93147ac3e566ce21ee38df8c8
Ayesha Quazi <134998217+Ayeshaq04@users.noreply.github.com>
2024-07-21 05:53:24 +0000

    Data-types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version, nowadays we don't need to write this, it's actully there. 
+
+// alert(3+3) we are using Node.js not browser!
+
+console.log("Ayesha!");
+// we must make our code readable. 
+
+// ECMA : Standard for JavaScript.
+// MDN: used for documentation.
+
+// DATA_TYPES
+
+let name = "Ayesha";
+let age = 20;
+let isStudent = true;
+
+
+// number : range [2 to the power of 53]
+// bigint : consumes large numbers
+// string : ".."
+// boolean : True/False
+// null : standalone value. [a spcl type] (Khali hai) {is an object when 'typeof'}
+// undefined : value is not assigned.
+// symbol : to find uniqueness
+
+// object
+
+console.log(typeof "ayesha");
