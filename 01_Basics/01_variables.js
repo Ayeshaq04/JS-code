d66bb28bb065ce5b41f42b72e56e7d797ff62ff0
Ayesha Quazi <134998217+Ayeshaq04@users.noreply.github.com>
2024-07-21 04:45:33 +0000

    variables used

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const account = 1234;
+let accountEmail = "ayesha@google.com";
+var accountPass = "1234";
+let accountState; // this var's value came as 'undefined' as we had not added any value to it. 
+accountCity = "A'bad"; // we can also use a variable_name without any keywords. But this is not recommended.
+
+// Also we should avoid using 'var' keyword for future uses. Instead we should use "let" keyword. 
+
+// account = 23452; This value can'y be changed! 
+
+accountEmail = "ayewsha@aa.com";
+accountPass = "9783";
+accountCity = "Pune";
+
+console.table([account, accountEmail, accountPass, accountCity, accountState]);
