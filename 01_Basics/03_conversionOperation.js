25d539f97515176402521ad2d229008c726c41fc
Ayesha Quazi <134998217+Ayeshaq04@users.noreply.github.com>
2024-07-22 06:14:49 +0000

    conversions of DT's

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = '37';
+
+console.log(typeof score);
+
+let valueInNum = Number(score) // The first letter must be in Caps.
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+// "37" => 37
+// "37ab" => Nan [Not a Number]
+// null => 0
+// undefined => NaN
+// boolean value: True-1; False-0
+// "" => false
+// "ayesha" => true
+
+let someNumber = 77;
+let stringNum = String(someNumber);
+console.log(stringNum);
+console.log(typeof stringNum);
+
