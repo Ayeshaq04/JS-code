c314fef671d3ef08c182ea12735fdb2e0f5b9d29
Ayesha Quazi <134998217+Ayeshaq04@users.noreply.github.com>
2024-07-24 17:56:51 +0000

    Strings

---
+++ 01_Basics/07_String.js
@@ -0,0 +1,16 @@
+const myName = new String('Ayeshaa')
+
+console.log(myName.length);
+console.log(myName.toUpperCase());
+console.log(myName.indexOf('y'));
+
+const newString = myName.substring(3,7)
+console.log(newString); //here we can't use -ve values.
+
+const newStr = myName.slice(-3,7)
+console.log(newStr);
+// here in slice, we can also give -ve values.
+
+const newStringOne = "   ayesha  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
