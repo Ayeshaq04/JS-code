a2fb6c7c15d3b7930664853cd3607beb43824ec7
Ayesha Quazi <134998217+Ayeshaq04@users.noreply.github.com>
2024-07-24 03:30:07 +0000

    DT-Summary

---
+++ 01_Basics/05_DTSummary.js
@@ -0,0 +1,25 @@
+// KIM => JS is a dynamic lang
+
+
+// Primitive DT [07]
+
+/* String, Number, Boolean, null, undefined, BigInt, Symbol(used when want to make a value unique) */
+
+const score = 100;
+const scoreVal = 100.9;
+
+let userName;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+const bigNum = 34374208943498392n;
+
+
+// Non-Primitive DT [Reference]
+
+/* Array, Objects, Functions*/ 
+
+const heros = ["Shaktiman", "Spiderman", "Batman"];
