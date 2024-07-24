93f3e74a171ed51e955f1606e1905e88670385eb
Ayesha Quazi <134998217+Ayeshaq04@users.noreply.github.com>
2024-07-24 03:16:39 +0000

    Comparision in js!

--- 01_Basics/04_comparision.js
+++ 01_Basics/04_comparision.js
@@ -1,6 +1,7 @@
-console.log(2 < 1);
-console.log(2 > 1);
-console.log(2 <= 1);
-console.log(2 == 1);
-console.log(2 !== 1);
+// here we won't get any issue, rather if we compare 2 diff DT's that would make problem to solve. 
+ 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
 
+// comparision converts null to a number, treating it as 0. 
