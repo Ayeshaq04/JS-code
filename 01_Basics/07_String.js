d54905d0c722a3e46e4b37a9eafa586f387ba250
Ayesha Quazi <134998217+Ayeshaq04@users.noreply.github.com>
2024-07-24 17:58:30 +0000

    changes made

--- 01_Basics/07_String.js
+++ 01_Basics/07_String.js
@@ -7,6 +7,8 @@ console.log(myName.indexOf('y'));
 const newString = myName.substring(3,7)
 console.log(newString); //here we can't use -ve values.
 
+console.log(`Hello my name is ${myName}!`)
+
 const newStr = myName.slice(-3,7)
 console.log(newStr);
 // here in slice, we can also give -ve values.
